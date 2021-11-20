f0bbeb2f0b722af857150f37145a47cb69260db6
andjamba <aurelien.ndjamba@gmail.com>
2021-11-20 18:04:03 +0100

    /api/configuration/SpringSecurityConfig.java -> Suppression de la deuxieme classe de config de spring sécurité crée /api/security/SecurityConfig.java -> Ajout de commentaire sur la configuration des filtres spring sécurité

--- src/main/java/com/paymybuddy/api/security/SecurityConfig.java
+++ src/main/java/com/paymybuddy/api/security/SecurityConfig.java
@@ -11,50 +11,96 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+/*
+ * @Configuration -> Indique à srping boot qu'il s'agit d'une classe de config -
+ * classe traité au démarrage de l'app
+ * 
+ * @EnableWebSecurity -> Indique à spring sécurity de savoir ou se trouve la
+ * configuration web Etendre cette classe va permettre de gérer notre chaine de
+ * filtre
+ * 
+ * WebSecurityConfigurerAdapter -> Pour personnaliser Spring sécurity, on a besoin d'hériter d'une classe
+ *  WebSecurityConfigurerAdapter
+ */
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
+	// @Autowired // -> JDBCAUTHENTICATION Source de donnée injectée comme bean
+	// private DataSource dbPayMyBuddy;
+
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-//		super.configure(auth);
-//		PasswordEncoder passwordEncoder = passwordEncoder();
-//		System.out.println("--------------------------------------");
-//		System.out.println(bCryptPasswordEncoder.encode("SarkozyN"));
-//		System.out.println(bCryptPasswordEncoder.encode("HollandeF"));
-//		System.out.println(bCryptPasswordEncoder.encode("MacronN"));
-//		System.out.println(bCryptPasswordEncoder.encode("ChiracJ"));
-//		System.out.println(bCryptPasswordEncoder.encode("MitterandF"));
-//		System.out.println(bCryptPasswordEncoder.encode("PompidouG"));
-//		System.out.println("--------------------------------------");
-//		auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder.encode("123")).roles("USER");
-//		auth.inMemoryAuthentication().withUser("aurelien").password(passwordEncoder.encode("cmr")).roles("USER","ADMIN");
-	
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// INMEMORYAUTHENTICATION
+//		auth.inMemoryAuthentication().withUser("user").password(bCryptPasswordEncoder.encode("password")).roles("USER");
+//		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+//		auth.inMemoryAuthentication().withUser("user").password(bCryptPasswordEncoder.encode("password")).roles("USER");
+//		auth.inMemoryAuthentication().withUser("user").password("{BCrypt}password").roles("USER");
+		// JDBCAUTHENTICATION
+//		auth.jdbcAuthentication().dataSource(dbPayMyBuddy).usersByUsernameQuery(
+//				"select email as principal, password as credentials, active as active from appuser where email=?")
+//				.authoritiesByUsernameQuery("SELECT email as principal, rolename as role FROM userRole WHERE email=?")
+//				.passwordEncoder(bCryptPasswordEncoder);
+
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
 	}
-	
+
+	/*
+	 * Pour gérer nos filtres, nous avons besoin de declarer la méthode configure
+	 * qui prend en entrée un requete http - HttpSecurity -> correspond au requete
+	 * http entrante 
+	 * 
+	 * @Override -> Pour que notre méthode prenne le pas sur celle de
+	 * spring security
+	 */
 	@Override
-	protected void configure(HttpSecurity http) throws Exception{
+	protected void configure(HttpSecurity http) throws Exception {
 ////		super.configure(http);
 		http.csrf().disable();
 		http.formLogin();
 //		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 ////		http.httpBasic();
-		
+
 //		http.authorizeRequests().antMatchers("/users/save","/users/delete**").permitAll();
 //		http.addFilter(new JWTAuthenticationFilter(authenticationManager()));
 //		http.authorizeRequests().antMatchers(HttpMethod.GET,"/accounts/all").hasRole("ADMIN");
 //		http.authorizeRequests().antMatchers(HttpMethod.GET,"/accounts/account").hasRole("ADMIN");
 //		http.authorizeRequests().anyRequest().authenticated(); // toute les ressources necessite une authentificaiton
 
-		//		http.authorizeRequests().antMatchers("/accounts/delete**").hasRole("ADMIN");
+		// http.authorizeRequests().antMatchers("/accounts/delete**").hasRole("ADMIN");
 ////		http.authorizeRequests().antMatchers("/accounts/delete**").permitAll(); toute les requetes machantes sont autorisé
 //		http.exceptionHandling().accessDeniedPage("/notAuthorized");
+
+//		// Pour gérer nos filtres, nous avons besoin de declarer la méthode configure
+//		// qui prend en entrée un requete http - HttpSecurity -> correspond au requete
+//		// http entrante
+		//
+//		@Override // Pour que notre méthode prenne le pas sur celle de spring security
+//		public void configure(HttpSecurity http) throws Exception {
+//			
+//			http.authorizeRequests().antMatchers("/admin/**").hasAuthority("ADMIN").and().formLogin(); //GOOD
+//			http.authorizeRequests().antMatchers("/user/**").hasAuthority("USER").and().formLogin();//GOOD
+
+//			http.formLogin(); // loginFocrm() -> Methode Spring security pour créer une page de connexion par
+		// défaut, vous évitant ainsi de la créer vous-même
+//			http.authorizeRequests().anyRequest().permitAll(); //OK sans login
+//			http.authorizeRequests().anyRequest().denyAll(); //OK error 403
+//			http.authorizeRequests().antMatchers("/**").authenticated(); //OK error 403
+//			http.authorizeRequests().antMatchers("/**").authenticated().and().formLogin(); //OK
+//			http.authorizeRequests().anyRequest().authenticated(); //OK error 403
+//			http.authorizeRequests().anyRequest().authenticated().and().formLogin(); //OK
+//			http.authorizeRequests().anyRequest().hasRole("ADMIN"); //OK error 403
+//			http.authorizeRequests().anyRequest().hasRole("ADMIN").and().formLogin(); //OK error 403 for USER
+//			http.authorizeRequests().anyRequest().hasRole("USER").and().formLogin(); //error 403 for USER ???
+//			http.authorizeRequests().anyRequest().hasAuthority("USER").and().formLogin(); //OK for USER
+//			http.authorizeRequests().anyRequest().hasAuthority("USER").and().formLogin(); //OK for macron who USER & ADMIN
+//			http.authorizeRequests().anyRequest().hasAuthority("ADMIN").and().formLogin(); //OK error 403 for USER
+//			http.authorizeRequests().anyRequest().hasAuthority("ADMIN").and().formLogin(); //OK for ADMIN
+//			http.csrf().disable();
 	}
-	
+
 }
