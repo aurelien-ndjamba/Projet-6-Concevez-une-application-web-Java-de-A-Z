4c834db74e37006932210c50bf6e307af12f5080
andjamba <aurelien.ndjamba@gmail.com>
2021-11-20 17:46:09 +0100

    Test unitaire sur controller OK -> Couverture 76% 	[WARNING] Tests run: 140, Failures: 0, Errors: 0, Skipped: 5
    	---------------------------pom.xml--------------------------------
        <java.version>14</java.version> modification depuis 15
    	-------------------AccountController.java------------------------
        - uri des rests controller remis comme à l'initial
        - la mise en commentaire de l'ensemble du fichier a été annulée
    	--------------------UserDetailsServiceImpl.java------------------
        la mise en commentaire de l'ensemble du fichier a été annulée
        ------------------------------------------------------------------

--- src/main/java/com/paymybuddy/api/service/UserDetailsServiceImpl.java
+++ src/main/java/com/paymybuddy/api/service/UserDetailsServiceImpl.java
@@ -1,36 +1,36 @@
-//package com.paymybuddy.api.service;
-//
-//import java.util.ArrayList;
-//import java.util.Collection;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//
-//import com.paymybuddy.api.model.AppUser;
-//
-//@Service
-//public class UserDetailsServiceImpl implements UserDetailsService{
-//
-//	@Autowired 
-//	UserService userService;
-//	@Autowired 
-//	RoleService roleService;
-//	
-//	@Override
-//	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-//		AppUser appUser = userService.findAllByEmail(email);
-//		if (appUser == null) throw new UsernameNotFoundException(email);
-//		Collection<GrantedAuthority> authorities = new ArrayList<>();
-//		appUser.getRoles().forEach (r -> {
-//			authorities.add(new SimpleGrantedAuthority(r.getRoleName()));
-//		});
-//		return new User(appUser.getEmail(),appUser.getPassword(),authorities);
-//	}
-//
-//}
+package com.paymybuddy.api.service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.paymybuddy.api.model.AppUser;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+	@Autowired 
+	UserService userService;
+	@Autowired 
+	RoleService roleService;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		AppUser appUser = userService.findByEmail(email);
+		if (appUser == null) throw new UsernameNotFoundException(email);
+		Collection<GrantedAuthority> authorities = new ArrayList<>();
+		appUser.getRoles().forEach (r -> {
+			authorities.add(new SimpleGrantedAuthority(r.getRoleName()));
+		});
+		return new User(appUser.getEmail(),appUser.getPassword(),authorities);
+	}
+
+}
